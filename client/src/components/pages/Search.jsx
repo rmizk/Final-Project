c2889f309373c35cee193693c32e723c09810376
Hammami Ramzi <rmizkk@gmail.com>
2022-03-02 11:39:19 +0100

    fix stuff

--- client/src/components/pages/Search.jsx
+++ client/src/components/pages/Search.jsx
@@ -1,11 +1,10 @@
-import { set } from "mongoose";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { search } from "../../redux/actions";
 import "./Search.css";
 
 const Search = () => {
-  const { list } = useSelector((state) => state.listreducer);
+  const { list, loading } = useSelector((state) => state.listreducer);
 
   const [membre, setmembre] = useState("");
   //   const [filteredList, setFilteredList] = useState([])
@@ -16,7 +15,7 @@ const Search = () => {
 
   useEffect(() => {
     dispatch(search());
-  }, []);
+  }, [membre, dispatch]);
 
   // useEffect(() => {
 
@@ -29,22 +28,37 @@ const Search = () => {
   return (
     <div>
       <div className="serach_box">
-        <h2> Rechercher un memebre </h2>
+        <h2> Rechercher un membre </h2>
         <input type="text" value={membre} onChange={filterMember} />
       </div>
-      {list
-        .filter(
-          (m) =>
-            m.fName.toLowerCase().includes(membre.toLowerCase()) ||
-            m.lName.toLowerCase().includes(membre.toLowerCase())
-        )
-        .map((el) => (
-          <div>
-            <h1> {el.lName + " " + el.fName} </h1>
-            <h4> Email : {el.email}</h4>
-            <h4> Phone : {el.phone}</h4>
-          </div>
-        ))}
+      {loading ? (
+        <h2> </h2>
+      ) : (
+        <>
+          {list
+            ?.filter(
+              (m) =>
+                m.fName.toLowerCase().includes(membre.toLowerCase()) ||
+                m.lName.toLowerCase().includes(membre.toLowerCase())
+            )
+            .map((el) => (
+              <div>
+                {/* <div className="image_container">
+                <div className="imagetest">
+                <img src="" alt="" />
+                </div>
+              </div> */}
+                <h1> {el.lName + " " + el.fName} </h1>
+                <h4> Email : {el.email}</h4>
+                <h4> Phone : {el.phone}</h4>
+              </div>
+            ))}
+        </>
+      )}
+
+      <br />
+      <br />
+      <br />
     </div>
   );
 };
